c79eb3afebd5af55e3d5cbeb233fd6b6faeca7d1
Andre_G <andre3000pt@gmail.com>
2020-10-02 19:23:34 +0100

    update bash

--- Lab_1/Aula_1/run.bash
+++ Lab_1/Aula_1/run.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
-# run as "sudo ./run.bash"
+
+# 1º uso
+# Abrir um terminal -> 3 tracinhos -> Preferencias -> Adicionar um profile e chamar "RCOM" -> tab "Command" -> When command exits: Hold the terminal open -> Done
+
+# Usos normais
+# ./run.bash
+# escrever palavra-passe de sudo | Tens 5 segundos para escrever - Ou mudas o valor do sleep de baixo
+# deixar andar
 
 gnome-terminal --window-with-profile=RCOM -- sudo socat -d  -d  PTY,link=/dev/ttyS10,mode=777   PTY,link=/dev/ttyS11,mode=777
 sleep 5

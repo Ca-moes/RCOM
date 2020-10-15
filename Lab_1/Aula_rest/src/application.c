3be814657cd4dbcd85b4a1d63b6afb8edc53727f
Andre_G <andre3000pt@gmail.com>
2020-10-15 19:38:48 +0100

    compilando

--- Lab_1/Aula_rest/src/application.c
+++ Lab_1/Aula_rest/src/application.c
@@ -3,6 +3,9 @@
 // Aplicação receberá como argumento em main se é Receiver ou Sender
 // com isto abrirá llopen() de forma diferente 
 int main(int argc, char** argv) {
+  // argv[1] = receiver/sender
+  // argv[2] = valor do x - "/dev/ttySx"
+
   int fd;
 
   printf("Started App\n");
--- Lab_1/Aula_rest/src/logs.h
+++ Lab_1/Aula_rest/src/logs.h
@@ -22,33 +22,21 @@
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 
 // prints "message" in red!
-void log_error(char *arr){
-  fprintf(stdout, RED "%s\n" RESET, arr);
-}
+void log_error(char *arr);
 
 // prints "message" in yellow!
-void log_caution(char *arr){
-  fprintf(stdout, YELLOW "%s\n" RESET, arr);
-}
+void log_caution(char *arr);
 
 // prints "message" in green!
-void log_success(char *arr){
-  fprintf(stdout, GREEN "%s\n" RESET, arr);
-}
+void log_success(char *arr);
 
 // prints "message" in normal white
-void log_message(char *arr){
-  fprintf(stdout, "%s\n" , arr);
-}
+void log_message(char *arr);
 
 // prints "message - numb"
-void log_message_number(char *arr, int numb){
-  fprintf(stdout, "%d - %s\n" , numb,  arr);
-}
+void log_message_number(char *arr, int numb);
 
 // prints "Content: 0xHH"
-void log_hexa(unsigned char a){
-  fprintf(stdout, "Content: %#4.2x\n" , a);
-}
+void log_hexa(unsigned char a);
 
 #endif // LOGS_HEADER

30e92abc7f4eea580a147ff530f9b71a7e9406d4
Andre_G <andre3000pt@gmail.com>
2020-10-24 19:14:51 +0100

    falta acabar docs

--- Lab_1/Aula_rest/src/statemachine.h
+++ Lab_1/Aula_rest/src/statemachine.h
@@ -5,9 +5,12 @@
 #include "logs.h"
 #include "pla.h"
 
-enum stateMachineType {Supervision, Write, Read};
-enum stateMachineState {Start, FLAG_RCV, A_RCV, C_RCV, BCC_OK, DONE};
+enum stateMachineType {Supervision, Write, Read};   // Type of State Machine
+enum stateMachineState {Start, FLAG_RCV, A_RCV, C_RCV, BCC_OK, DONE};  // State of the State Machine
 
+/**
+ * @brief List of Parameters to Personalize the State Machine
+ */
 struct stateMachineParams {
   unsigned char control; // Control Byte that is suppossed to be received
   unsigned char address; // Address Byte that is suppossed to be received
@@ -15,12 +18,27 @@ struct stateMachineParams {
   enum stateMachineType type;
 };
 
-struct stateMachineParams state_machine;
-unsigned checkBuffer[2];
+struct stateMachineParams state_machine; // Instance of the stateMachineParams struct
+unsigned checkBuffer[2]; // Buffer to hold Address and Control Bytes to check BCC1
 int frameIndex, wrongC;
 
-
+/**
+ * @brief Function to easily change the State Machine
+ * 
+ * @param control Control Byte that is suppossed to be received
+ * @param address Address Byte that is suppossed to be received
+ * @param state State of the State Machine
+ * @param type Type of State Machine
+ */
 void stateMachineSetUp(unsigned char control, unsigned char address, enum stateMachineState state, enum stateMachineType type);
+/**
+ * @brief 
+ * 
+ * @param byte 
+ * @param buf 
+ * @param size 
+ * @return int 
+ */
 int stateMachine(unsigned char byte, unsigned char **buf, int *size);
 
 int processStart(unsigned char byte);

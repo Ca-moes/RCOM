e495d7e396f1258253ae9a786515865a18a0e5a7
Andre_G <andre3000pt@gmail.com>
2020-11-23 15:14:08 +0000

    lab2

---
+++ Lab_2/Docs/Código/clientTCP.c
@@ -0,0 +1,48 @@
+/*      (C)2000 FEUP  */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netdb.h>
+#include <strings.h>
+
+#define SERVER_PORT 6000
+#define SERVER_ADDR "192.168.28.96"
+
+int main(int argc, char** argv){
+
+	int	sockfd;
+	struct	sockaddr_in server_addr;
+	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	int	bytes;
+	
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+    perror("socket()");
+    exit(0);
+  }
+	/*connect to the server*/
+  if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+    perror("connect()");
+		exit(0);
+	}
+  /*send a string to the server*/
+	bytes = write(sockfd, buf, strlen(buf));
+	printf("Bytes escritos %d\n", bytes);
+
+	close(sockfd);
+	exit(0);
+}
+
+
---
+++ Lab_2/Docs/Código/getip.c
@@ -0,0 +1,40 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <errno.h> 
+#include <netdb.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+#include<arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+	struct hostent *h;
+
+        if (argc != 2) {  
+            fprintf(stderr,"usage: getip address\n");
+            exit(1);
+        }
+        
+        
+/*
+struct hostent {
+	char    *h_name;	Official name of the host. 
+  char    **h_aliases;	A NULL-terminated array of alternate names for the host. 
+	int     h_addrtype;	The type of address being returned; usually AF_INET.
+  int     h_length;	The length of the address in bytes.
+	char    **h_addr_list;	A zero-terminated array of network addresses for the host. 
+				Host addresses are in Network Byte Order. 
+};
+
+#define h_addr h_addr_list[0]	The first address in h_addr_list. 
+*/
+        if ((h=gethostbyname(argv[1])) == NULL) {  
+            herror("gethostbyname");
+            exit(1);
+        }
+
+        printf("Host name  : %s\n", h->h_name);
+        printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+
+        return 0;
+}
